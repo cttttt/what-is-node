6b2215e027338af21cbff17be674426e105e6ab7
Chris Taylor <cntaylor@ca.ibm.com>
2016-12-19 07:56:34 -0500

    ct: Allow the file, `name.txt` to override `name`.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-var name = process.argv[2] || process.env.NAME || 'Chris';
+var fs = require('fs');
+
+var name = process.argv[2] || process.env.NAME;
+
+try {
+    name = name || fs.readFileSync('name.txt').toString().trim();
+} catch (e) {}
+
+name = name || 'Chris';
+
 console.log('Hello, %s', name);
