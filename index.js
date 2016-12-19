3b9d8259b9655cb9b1325d4504469f1d627bc1ae
Chris Taylor <cntaylor@ca.ibm.com>
2016-12-19 09:44:30 -0500

    ct: Make it into an `http` server.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-var deriveName = require('./derive-name');
-console.log('Hello, %s', deriveName());
+var deriveName = require('./derive-name'),
+    http = require('http'),
+    util = require('util');
+
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'content-type': 'text/plain' });
+    var message = util.format('Hello, %s\n', deriveName());
+    res.end(message);
+});
+
+server.listen(process.env.PORT || 30303);
